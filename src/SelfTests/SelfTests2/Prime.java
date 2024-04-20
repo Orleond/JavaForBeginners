648104be5c65ab7d8b82fe584cf8f01ffdb35aae
Mr. Orleond <mr.orleond@gmail.com>
2024-04-20 15:49:55 +0300

    Chapter 2

---
+++ src/SelfTests/SelfTests2/Prime.java
@@ -0,0 +1,22 @@
+package SelfTests.SelfTests2;
+
+/**
+ * Нахождение простых чисел в диапазоне от 2 до 100
+ */
+public class Prime {
+    public static void main(String args[]) {
+        int i, j;
+        boolean isprime;
+        for (i = 2; i < 100; i++) {
+            isprime = true;
+
+            // Проверить, делится ли число без остатка
+            for (j = 2; j <= i / j; j++)
+                // Если число делится без остатка, значит, оно не простое
+                if ((i % j) == 0) isprime = false;
+
+            if (isprime)
+                System.out.println(i + " - простое число.");
+        }
+    }
+}

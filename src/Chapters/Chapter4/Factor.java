216ae59ced75ff2044307a39979c30625f1e05b2
Mr. Orleond <mr.orleond@gmail.com>
2024-04-20 21:15:52 +0300

    Chapter 4

---
+++ src/Chapters/Chapter4/Factor.java
@@ -0,0 +1,11 @@
+package Chapters.Chapter4;
+
+/**
+ * Применение метода с двумя параметрами
+ */
+public class Factor {
+    boolean isFactor(int a, int b) {
+        if ((b % a) == 0) return true;
+        else return false;
+    }
+}

26dfe95ec74bcae31924a96d8a4cf2826569a847
Mr. Orleond <mr.orleond@gmail.com>
2024-04-20 22:38:44 +0300

    Chapter 5

--- src/Exercises/Exercises5/ShowBitsDemo.java
+++ src/Exercises/Exercises5/ShowBitsDemo.java
@@ -3,32 +3,6 @@ package Exercises.Exercises5;
  * Упражнение 5.3
  * Создание класса для отображения значений в двоичном виде
  */
-class ShowBits {
-    int numBits;
-
-    ShowBits(int numBits) {
-        this.numBits = numBits;
-    }
-
-    void show(long val) {
-        long mask = 1;
-
-        // Сдвиг значения 1 влево на нужную позицию
-        mask <<= numBits-1;
-
-        int spacer = 0;
-        for (; mask != 0; mask >>>= 1) {
-            if ((val & mask) != 0) System.out.print("1");
-            else System.out.print("0");
-            spacer++;
-            if ((spacer % 8) == 0) {
-                System.out.print(" ");
-                spacer = 0;
-            }
-        }
-        System.out.println();
-    }
-}
 public class ShowBitsDemo {
     public static void main(String[] args) {
         ShowBits b = new ShowBits(8);

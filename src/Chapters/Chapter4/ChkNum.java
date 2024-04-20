b8d442250ebe0bb28c0811caaba4d0502eeb599c
Mr. Orleond <mr.orleond@gmail.com>
2024-04-20 21:10:10 +0300

    Chapter 4

---
+++ src/Chapters/Chapter4/ChkNum.java
@@ -0,0 +1,12 @@
+package Chapters.Chapter4;
+/*
+ * Простой пример применения параметра в методе
+ */
+class ChkNum {
+    // Возврат логического значения true,
+    // если x содержит четное число
+    boolean isEven(int x) {
+        if ((x % 2) == 0) return true;
+        else return false;
+    }
+}
--- src/Chapters/Chapter4/ParmDemo.java
+++ src/Chapters/Chapter4/ParmDemo.java
@@ -2,14 +2,6 @@ package Chapters.Chapter4;
 /*
  * Простой пример применения параметра в методе
  */
-class ChkNum {
-    // Возврат логического значения true,
-    // если x содержит четное число
-    boolean isEven(int x) {
-        if ((x % 2) == 0) return true;
-        else return false;
-    }
-}
 public class ParmDemo {
     public static void main(String[] args) {
         ChkNum e = new ChkNum();

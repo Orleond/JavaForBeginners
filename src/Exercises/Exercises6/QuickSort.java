26237c1dfdf516d65b4f71ab31612831f7ebf0ef
Mr. Orleond <mr.orleond@gmail.com>
2024-04-20 23:55:12 +0300

    Chapter 6

---
+++ src/Exercises/Exercises6/QuickSort.java
@@ -0,0 +1,36 @@
+package Exercises.Exercises6;
+/*
+ * Упражнение 6.3
+ * Простая версия класса Quicksort,
+ * реализующего быструю сортировку
+ */
+class Quicksort {
+    // Вызов фактического метода быстрой сортировки
+    static void qsort(char[] items) {
+        qs(items, 0, items.length-1);
+    }
+
+    // Рекурсивная версия метода быстрой сортировки
+    private static void qs(char[] items, int left, int right) {
+        int i, j;
+        char x, y;
+
+        i = left; j = right;
+        x = items[(left+right)/2];
+
+        do {
+            while ((items[i] < x) && (i < right)) i++;
+            while ((x < items[j]) && (j > left)) j--;
+
+            if (i <= j) {
+                y = items[i];
+                items[i] = items[j];
+                items[j] = y;
+                i++; j--;
+            }
+        } while ((i <= j));
+
+        if (left < j) qs(items, left, j);
+        if (i < right) qs(items, i, right);
+    }
+}

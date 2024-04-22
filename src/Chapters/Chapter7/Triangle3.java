93cdf625b8647a97ece2ab99adc2fa3e23e0d7eb
Mr. Orleond <mr.orleond@gmail.com>
2024-04-22 12:47:28 +0300

    Chapter 7

--- src/Chapters/Chapter7/Triangle3.java
+++ src/Chapters/Chapter7/Triangle3.java
@@ -4,8 +4,16 @@ package Chapters.Chapter7;
  * Подкласс для представления треугольников,
  * производный от класса TwoDShape
  */
-public class Triangle2 extends TwoDShape2 {
-    String style;
+public class Triangle3 extends TwoDShape2 {
+    private String style;
+
+    // Конструктор
+    Triangle3(String s, double w, double h) {
+        setWidth(w);
+        setHeight(h);
+
+        style = s;
+    }
 
     public double area() {
         return getWidth() * getHeight() / 2;

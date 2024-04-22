12a28ef3c175acf4754f194c5cbc44a5d5ced232
Mr. Orleond <mr.orleond@gmail.com>
2024-04-22 19:09:10 +0300

    Chapter 8

---
+++ src/Chapters/Chapter8/Series3.java
@@ -0,0 +1,34 @@
+package Chapters.Chapter8;
+/**
+ * Еще одна расширенная версия интерфейса Series, включающая
+ * два заданных по умолчанию метода и использующая закрытый
+ * метод getArray();
+ */
+public interface Series3 {
+    int getNext();         // Возвращает следующее число в ряду
+
+    // Возврат массива, который содержит n элементов,
+    // располагающихся в ряду вслед за текущим элементом
+    default int[] getNextArray(int n) {
+        return getArray(n);
+    }
+
+    // Возврат массива, содержащего следующие n элементов,
+    // в ряду, после пропуска элементов
+    default int[] skipAndGetNextArray(int skip, int n) {
+        // Пропуск указанного числа элементов
+        getArray(skip);
+        return getArray(n);
+    }
+
+    // Закрытый метод, возвращающий массив, который
+    // содержит следующие n элементов
+    private int[] getArray(int n) {
+        int[] vals = new int[n];
+
+        for (int i = 0; i < n; i++) getNext();
+        return vals;
+    }
+    void reset();           // Сброс
+    void setStart(int x);   // Установка начального значения
+}

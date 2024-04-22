5b644f18c83c275cc3d9b5b48db0e5d27c270d96
Mr. Orleond <mr.orleond@gmail.com>
2024-04-22 12:55:06 +0300

    Chapter 7

---
+++ src/Chapters/Chapter7/Triangle4.java
@@ -0,0 +1,23 @@
+package Chapters.Chapter7;
+
+/**
+ * Подкласс для представления треугольников,
+ * производный от класса TwoDShape
+ */
+public class Triangle4 extends TwoDShape3 {
+    private String style;
+
+    Triangle4(String s, double w, double h) {
+        super(w, h);    // Вызов конструктора суперкласса
+
+        style = s;
+    }
+
+    public double area() {
+        return getWidth() * getHeight() / 2;
+    }
+
+    void showStyle() {
+        System.out.println("Треугольник " + style);
+    }
+}
---
+++ src/Chapters/Chapter7/TwoDShape3.java
@@ -0,0 +1,26 @@
+package Chapters.Chapter7;
+
+/**
+ * Добавление конструкторов в класс TwoDShape
+ */
+public class TwoDShape3 {
+    private double width;
+    private double height;
+
+    // Параметризированный конструктор
+    TwoDShape3(double w, double h) {
+        width = w;
+        height = h;
+    }
+
+    // Методы доступа к закрытым переменным экземпляра width и height
+    protected double getWidth() { return width; }
+    double getHeight() { return height; }
+    void setWidth(double w) { width = w; }
+    void setHeight(double h) { height = h; }
+
+    void showDim() {
+        System.out.println("Ширина и высота - " +
+                width + " и " + height);
+    }
+}

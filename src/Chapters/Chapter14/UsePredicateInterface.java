3172527fae2a976422a74b2481f9fd8449684a64
Mr. Orleond <mr.orleond@gmail.com>
2024-04-23 21:59:34 +0300

    Chapter 14

--- src/Chapters/Chapter14/UsePredicateInterface.java
+++ src/Chapters/Chapter14/UsePredicateInterface.java
@@ -1,11 +1,9 @@
 package Chapters.Chapter14;
 
-// Импортировать интерфейс Predicate
 import java.util.function.Predicate;
 /**
  * Использование встроенного функционального интерфейса Predicate
  */
-
 public class UsePredicateInterface {
     public static void main(String[] args) {
         // Данное лямбда-выражение использует интерфейс
